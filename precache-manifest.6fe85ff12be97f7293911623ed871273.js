c6f776c6eb3e9e5619322afe054f1d7ca11ad4b9
Aris <arisapostoloy@gmail.com>
2020-03-19 01:33:20 +0200

    Updates

---
+++ precache-manifest.6fe85ff12be97f7293911623ed871273.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0b284fd0aca598f67fd",
+    "url": "/ArisApos/SuperTicTacToe/static/js/runtime~main.c0b284fd.js"
+  },
+  {
+    "revision": "ac304f3b0f70cec5f618",
+    "url": "/ArisApos/SuperTicTacToe/static/js/main.ac304f3b.chunk.js"
+  },
+  {
+    "revision": "cccd8235db7090802a66",
+    "url": "/ArisApos/SuperTicTacToe/static/js/1.cccd8235.chunk.js"
+  },
+  {
+    "revision": "ac304f3b0f70cec5f618",
+    "url": "/ArisApos/SuperTicTacToe/static/css/main.24105153.chunk.css"
+  },
+  {
+    "revision": "1e70e9487c075c6405eca6388d9d0e09",
+    "url": "/ArisApos/SuperTicTacToe/index.html"
+  }
+];
