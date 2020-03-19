d51b528edf1c3cbccf2ce367438a7766cc56461d
Aris <arisapostoloy@gmail.com>
2020-03-19 02:27:44 +0200

    Updates

---
+++ precache-manifest.80ff3f766ad496408777a7b01dcd33b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7ee0c586be53253016e7",
+    "url": "/static/js/main.7ee0c586.chunk.js"
+  },
+  {
+    "revision": "cccd8235db7090802a66",
+    "url": "/static/js/1.cccd8235.chunk.js"
+  },
+  {
+    "revision": "7ee0c586be53253016e7",
+    "url": "/static/css/main.0b29b049.chunk.css"
+  },
+  {
+    "revision": "9525e609da8f6c60690a74a3d97031c9",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ArisApos/SuperTicTacToe/precache-manifest.bbe07fd1a45428e0845d82da9f4d82c6.js"
+  "/precache-manifest.80ff3f766ad496408777a7b01dcd33b7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ArisApos/SuperTicTacToe/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
